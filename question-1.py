6e75535c2ab07145be4b6ca080ee2f116aad6b54
Ketki-Karandikar <167843853+Ketki-Karandikar@users.noreply.github.com>
2024-05-06 09:44:44 +0530

    Update question-1.py

--- question-1.py
+++ question-1.py
@@ -1,13 +1,23 @@
-# Write a python program to test whether a passed letter is a vowel or not ?
+# Write a python program to check if a number is positive, negative or zero ?
 
+"""
+n=int(input("Enter number : "))
+x=["positive" if n>0 else "negative" if n<0 else "zero" ]    # For Eqaual to zero number will print Eqaual to zero
+print(x)                                                     # For positive number will print positive
+                                                             # For negative number will print negative
+"""                                               
 
-# Function to check whether a character is vowel or consonant 
+# Used If elif and else loop 
 
-x = (input("Enter letter : "))
-if (x == 'a' or x == 'e' or x == 'i' or x == 'o' or x == 'u'): 
-  print("Vowel") 
+n=int(input("Enter number : "))    # Input number or initialize
+
+if n>0:
+    print("Positive")     # For positive number will print positive
+
+elif n<0:
+    print("Negative")    # For negative number will print negative
+
 else:
-  print("Consonant") 
-
+    print("Neither negative nor positive.")  # For Eqaual to zero number will print Eqaual to zero
 
 
