5350b529a83aa10eba917e98af29c2d038f71a35
ketkikarandikar <89449954+ketkikarandikar@users.noreply.github.com>
2024-05-29 14:22:35 +0530

    Delete Assignments directory

--- Assignments/Module_2/question--10.py
+++
@@ -1,9 +0,0 @@
-# Write a Python program that will return true if the two given integer values are equal or their sum 
-# or difference is 5 ?
-x = int(input("Enter num1 : "))
-y = int(input("Enter num2 : "))   # Boolean type
-                                               
-if x==y or abs(x - y) == 5 or (x + y) == 5:     # will print true if the addition or substraction of the values
-    print("True")                               # are equal to 5
-else:                                           # or will print False
-    print("False")
--- Assignments/Module_2/question--12.py
+++
@@ -1,5 +0,0 @@
-# Write a Python program to calculate the length of a string ?
-
-k = "Today is 23rd April 2024" 
-
-print(len(k))   # len is used to print the length of the string
